69b606d8365785082655b219e4a8cb99dffca681
Lucas Fogaça <lucas.fogaca@vsoft.com.br>
2024-08-26 20:01:42 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.querySelector('#menu ul');
+  
+    menuToggle.addEventListener('click', () => {
+      menu.classList.toggle('disable');
+    });
+  
