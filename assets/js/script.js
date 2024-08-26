99003597e7e9f6168b0abf1fbd3030643931c28e
Lucas Fogaça <lucas.fogaca@vsoft.com.br>
2024-08-26 20:35:51 -0300

    ad spa

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,14 @@
     menuToggle.addEventListener('click', () => {
       menu.classList.toggle('disable');
     });
+
+    function navigate(page){
+      document.querySelectorAll('main > section').forEach(section => {
+        section.classList.add('disable');
+      });  
+
+      document.getElementById(page).classList.remove('disable');
+    }
+
+
   
