566fd83380fb1e241df744d31f4766a62be59a51
Lucas Fogaça <lucas.fogaca@vsoft.com.br>
2024-08-26 21:26:25 -0300

    objeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,16 @@
     function navigate(page){
       document.querySelectorAll('main > section').forEach(section => {
         section.classList.add('disable');
-      });  
-
+      });
       document.getElementById(page).classList.remove('disable');
     }
 
+    function submit(){
+      const contato = {
+        nome : document.getElementById('nome').value,
+        telefone : document.getElementById('phone').value,
+        email : document.getElementById('email').value
+      };
+    }
 
   
